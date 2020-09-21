868e38523d47e4dd2305bc9dc78c719758e072c9
LiQuinten <q.j.leeflang@gmail.com>
2020-09-21 11:43:09 +0200

    Logout fix eindelijk
    Logout is nu eindelijk gefixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,17 +49,21 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                            </li>
                             <li class="nav-item dropdown">
+
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
@@ -76,5 +80,6 @@
             @yield('content')
         </main>
     </div>
+
 </body>
 </html>
